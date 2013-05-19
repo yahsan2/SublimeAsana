7ac54c2c5a4b2cd2131a71f4dae8707bf51bfa15
miyamoto ryosuke <yahsan2@gmail.com>
2013-05-20 07:30:45 +0900

    Add command

--- SublimeAsana.py
+++ SublimeAsana.py
@@ -39,29 +39,35 @@ class GetAsanaTasksCommand(sublime_plugin.TextCommand):
     def show_quick_panel_select(self,index):
         self.current_task_id = self.task_ids[index]
         self.current_task_name = self.task_names[index]
-        command = ['0: back','1: done','2: done & commit','3: update']
+        command = ['0: Back','1: Done','2: Done & Commit','3: Update','4: Cancel']
         self.view.window().show_quick_panel(command, self.command_task)
 
     def command_task(self,index):
         if index == 0 :
             self.show_quick_panel_task('cache')
-        elif index == 1 :
+        elif index == 1 or index == 2:
             thread = AsanaApiCall('done_task', int(self.current_task_id), self.on_done)
             thread.start()
-        elif index == 2 :
-            self.commit_task()
+            if index == 2:
+                self.view.window().show_input_panel('Commit -am: ', self.current_task_name+' #'+str(self.current_task_id), self.git_commit, None, None)
+        elif index == 3 :
+            self.view.window().show_input_panel('Change name: ', self.current_task_name, self.update_task, None, None)
+        elif index == 4 :
+            self.on_done()
 
-    def commit_task(self):
-        self.view.window().show_input_panel('Commit -am: ', self.current_task_name+' #'+str(self.current_task_id), self.git_commit, None, None)
-
-    def git_commit(self,text):
-        sublime.message_dialog(text)
-        self.view.window().run_command('exec', {'cmd': ['git', 'commit', '-am', text], 'quiet': False})
+    def update_task(self,name):
+        thread = AsanaApiCall('update_task', [int(self.current_task_id),name], self.on_done)
+        thread.start()
 
 
-    def on_done(self,name):
-        sublime.status_message('Done: '+name)
-        pass
+    def git_commit(self,message):
+        self.view.window().run_command('exec', {'cmd': ['git', 'commit', '-am', message], 'quiet': False})
+        sublime.status_message('Commit: '+message)
+
+
+    def on_done(self,name=False):
+        if name :
+            sublime.status_message('done '+ name)
 
 
 class SetAsanaProjectCommand(sublime_plugin.TextCommand):
@@ -79,12 +85,12 @@ class SetAsanaProjectCommand(sublime_plugin.TextCommand):
 
         self.view.window().show_quick_panel(self.project_names, self.save_project_id)
 
-
     def save_project_id(self,index):
         self.path = self.view.window().folders()[0]
         AsanaProjects.set(self.path, str(self.project_ids[index]))
         sublime.save_settings('AsanaProjects.sublime-settings')
 
+        self.view.window().run_command('get_asana_task')
 
 class AddAsanaTaskCommand(sublime_plugin.TextCommand):
     def run(self,edit):
@@ -103,21 +109,23 @@ class AddAsanaTaskCommand(sublime_plugin.TextCommand):
 
 class AsanaApiCall(threading.Thread):
     def __init__(self,command,args,callback):
-        asana_api_key = sublime.active_window().active_view().settings().get('asana_api_key')
-        self.AsanaApi = asana.AsanaAPI( asana_api_key , debug=True)
         self.command = command
         self.args = args
         self.callback = callback
         threading.Thread.__init__(self)
 
-    def main_thread(self,callback, args):
+    def main_thread(self,callback, args=False):
         # sublime.set_timeout gets used to send things onto the main thread
         # most sublime.[something] calls need to be on the main thread
         sublime.set_timeout(functools.partial(callback, args), 10)
 
 
     def run(self):
+        self.main_thread(self.api_command,10)
 
+    def api_command(self,na):
+            asana_api_key = sublime.active_window().active_view().settings().get('asana_api_key')
+            self.AsanaApi = asana.AsanaAPI( asana_api_key , debug=True)
         # try:
             if self.command == 'get_project_tasks':
                 tasks = self.AsanaApi.get_project_tasks(self.args)
@@ -132,6 +140,10 @@ class AsanaApiCall(threading.Thread):
                 task = self.AsanaApi.create_task(self.args[0], myspaces[1]['id'])
                 self.AsanaApi.add_project_task(task[u'id'], self.args[1])
                 self.main_thread(self.callback,self.args[0])
+
+            elif self.command == 'update_task':
+                task = self.AsanaApi.update_task(self.args[0], self.args[1])
+                self.main_thread(self.callback,task[u'name'])
 
             elif self.command == 'done_task':
                 task = self.AsanaApi.update_task(self.args, None, None, None, True)
